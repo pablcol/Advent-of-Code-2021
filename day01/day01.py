0a283ce211c6363a7c9455a1186ab10d2a3a9ed5
pablcol <pablcol@gmail.com>
2021-12-01 16:37:52 +0100

    solution to day1, PEP8 comply

--- day01/day01.py
+++ day01/day01.py
@@ -1,13 +1,26 @@
-def main():
-	#def sonar():
-	input = [""]
-	input = open("inputd1.txt").readlines()
-	for lines in input:
-		counter = 0
-		if int(lines) < int(lines)+1:
-			counter += 1
-			print(counter)
-	return
+# This takes some data in the form of a txt file, reads it separated by lines,
+# converts it to int values and then puts it in a python dictionary
 
-if __name__ == "__main__":
-	main()
+import numpy as np
+
+data = [int(n) for n in open("day01/inputd1.txt").readlines()]
+
+
+# This function for the first ex, takes the values stored in data, and checks
+# for any increase in two consecutive numbers, then adds them all together
+def ex01(d):
+    return sum((np.array(d) - np.append([0], np.array(d[:-1]))) > 0) - 1
+
+
+print(ex01(data))
+
+# This funciton for the 2nd ex takes the result of the conovlution (mod) of the
+# data, in 3 increments and passes it to the first function in order to get
+
+
+def increaseWindow(d, wsize=3):
+    w = np.convolve(np.array(d), np.ones(wsize), 'valid')
+    return ex01(w)
+
+
+print(increaseWindow(data))
