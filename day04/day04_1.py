aa8581fbc89e1f98517b0979e28fb601f341885d
Pau <pablcol@gmail.com>
2021-12-04 18:06:57 +0100

    1st attempt

---
+++ day04/day04_1.py
@@ -0,0 +1,46 @@
+file = open("day04/inputd4.txt")
+
+bingo_numbers = list(map(int, next(file).split(",")))
+
+boards = []
+for row in file:
+    row = row.strip()
+    if row == "":
+        board = []
+        boards.append(board)
+        continue
+    board += map(int, row.split())
+
+
+def wins(board: list) -> bool:
+    for i in range(0, 5):
+        if all(n < 0 for n in board[5 * i:5 * i + 5]):
+            return True
+        if all(n < 0 for n in board[i::5]):
+            return True
+    return False
+
+
+def first_winner():
+    for number in bingo_numbers:
+        for i in range(len(boards)):
+            boards[i] = [n if n != number else -1 for n in boards[i]]
+            if wins(boards[i]):
+                return number * sum(n for n in boards[i] if n >= 0)
+
+
+def last_winner():
+    for number in bingo_numbers:
+        winners = []
+        for i in range(len(boards)):
+            boards[i] = [n if n != number else -1 for n in boards[i]]
+            if wins(boards[i]):
+                if len(boards) == 1:
+                    return number * sum(n for n in boards[i] if n >= 0)
+                winners.append(boards[i])
+        for board in winners:
+            boards.remove(board)
+
+
+print(first_winner())
+print(last_winner())
