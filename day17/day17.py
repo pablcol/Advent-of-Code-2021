4ad0c5d85ed6b3b9bb2d04b1b1317b6aad3a6ecf
pablcol <pablcol@gmail.com>
2021-12-17 17:11:13 +0100

    solution to day17, PEP8 comply, final

--- day17/day17.py
+++ day17/day17.py
@@ -1,38 +1,17 @@
-import sys
-import heapq
-import itertools
-from collections import defaultdict, Counter, deque
-
-#target area: x=96..125, y=-144..-98
-
-# y(t) ~ -t^2 + DY*t
-
-p2 = 0
-ans = 0
-for DX in range(150):
-    for DY in range(-150, 1000):
-        ok = False
-        max_y = 0
-        x = 0
-        y = 0
-        dx = DX
-        dy = DY
-        for t in range(1000):
-            x += dx
-            y += dy
-            max_y = max(max_y, y)
-            if dx > 0:
-                dx -= 1
-            elif dx < 0:
-                dx += 1
-            dy -= 1
-            #if 20<=x<=30 and -10<=y<=-5:
-            if 96<=x<=125 and -144<=y<=-98:
-                ok = True
-        if ok:
-            p2 += 1
-            if max_y > ans:
-                ans = max_y
-                print(DX,DY,ans)
-print(ans)
-print(p2)
+file = open("day17/inputd17.txt", 'r')
+minx, maxx, miny, maxy = sum([[int(y) for y in x.split('=')[1].split('..')]
+                              for x in file.read().split(', ')], [])
+count, best = 0, 0
+for tx in range(1, maxx + 1):
+    for ty in range(miny, -maxy + (maxy - miny)):
+        x, y, h = 0, 0, 0
+        vx, vy = tx, ty
+        while vx > 0 or y > miny:
+            x, y, h = x + vx, y + vy, max(h, y)
+            vx, vy = max(0, vx - 1), vy - 1
+            if minx <= x <= maxx and miny <= y <= maxy:
+                count += 1
+                best = max(best, h)
+                break
+print(best)
+print(count)
